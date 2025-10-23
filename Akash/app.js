483f6a1e10a0eec050e62f736faab0d77f4ce825
Akash Dwivedi <akashdwi17@gmail.com>
2025-10-23 22:44:30 +0530

    js

---
+++ Akash/app.js
@@ -0,0 +1,61 @@
+// Arrays
+
+let students = ["aman", "shradha", "rajat"];
+console.log (students);
+
+let reversed = students.toReversed();
+console.log (reversed);
+
+// join
+let ans = students.join (" % ");
+console.log (ans);
+
+
+let marks = [99,98,"ak",3.14];
+console.log (marks);
+
+// Arrays
+
+let info = ["Akash", 20, 6.2];
+console.log(info);
+
+let empArr = [];
+console.log(empArr);
+
+console.log(info.length);
+
+console.log(info[0][0]);
+
+console.log(info[0].length);
+
+let fruits = ["mango", "apple", "litchi"];
+fruits[0] = "banana";
+console.log(fruits);
+
+fruits[1] = "orange";
+
+console.log(fruits);
+
+let car = ["audi", "bmw", "maruti", "xuv"];
+
+console.log (car);
+
+// shift  unshift push pop
+
+// let newCar = car.push ("farari");
+// console.log (newCar);
+ 
+// Note: push("farari") adds "farari" to the end of car.
+// But push() returns the new length of the array — not the updated arra
+
+car.push ("farari");
+console.log (car);
+
+car.pop();
+console.log (car);
+
+car.unshift ("lamborgini");
+console.log (car);
+
+car.shift ();
+console.log (car);
